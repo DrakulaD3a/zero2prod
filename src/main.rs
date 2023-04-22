5989d3dfeb645df9211132e3d32b2b0104401915
Lukas Strnad <strnad.lukas@proton.me>
2023-04-22 19:42:34 +0200

    Fixing formatting

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))})
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
