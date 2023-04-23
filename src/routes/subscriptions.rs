59c5db9df9cc30aaf9b2596a26b8a2a9560ca347
Lukas Strnad <strnad.lukas@proton.me>
2023-04-23 18:49:13 +0200

    Adding request id to logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,19 @@ pub struct FormData {
 }
 
 pub async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +36,16 @@ pub async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("request_id {} - New subscriber saved.", request_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            eprintln!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     };
