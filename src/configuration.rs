4dff66374edeb982352471309d2d74d9ed750c33
Lukas Strnad <strnad.lukas@proton.me>
2023-04-23 11:20:08 +0200

    Test isolation for db

--- src/configuration.rs
+++ src/configuration.rs
@@ -20,7 +20,14 @@ impl DatabaseSettings {
         format!(
             "postgres://{}:{}@{}:{}/{}",
             self.username, self.password, self.host, self.port, self.database_name
-       )
+        )
+    }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
     }
 }
 
