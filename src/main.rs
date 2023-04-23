d4ea619afa3f7151559339f9352eb8a9471420cf
Lukas Strnad <strnad.lukas@proton.me>
2023-04-23 10:38:54 +0200

    Adding all needed env files, tests failing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use zero2prod::run;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener =
-        std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to assign random port.");
+    let configuration = get_configuration().expect("Failed to read config.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
