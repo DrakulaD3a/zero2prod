62f8317e067e3e57765945f27f2971d1cb69d398
Lukas Strnad <strnad.lukas@proton.me>
2023-04-24 11:13:22 +0200

    Formatting

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,10 @@
-use tracing_actix_web::TracingLogger;
 use crate::routes::health_check;
 use crate::routes::subscriptions;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
